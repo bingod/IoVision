4af1f175127dc5d73bc7e550accfbf77369f52ef
bingod <cy@cy-Lenovo.(none)>
2014-10-08 18:06:30 +0800

    fu

--- src/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-handlers = []
-
-modules = ["index", "post", "about"]
-
-for module in modules:
-    handler = __import__("src.handlers." + module, fromlist="src.handlers")
-    handlers.append(handler.handler)
